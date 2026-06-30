446d317652c008bae6262dac3b3d5d5df7b0f77b
Your Name <you@example.com>
2026-06-30 16:42:32 +0200

    Cambios

--- Objetos/Ejercicio2/src/main/java/DiscoDuro.java
+++ Objetos/Ejercicio2/src/main/java/DiscoDuro.java
@@ -1,9 +1,9 @@
 public class DiscoDuro {
 private String marca;
-private TIpoDiscoDuro tipo;
+private TipoDiscoDuro tipo;
 private int capacidadEnGB;
 
-    public DiscoDuro(String marca, TIpoDiscoDuro tipo, int capacidadEnGB) {
+    public DiscoDuro(String marca, TipoDiscoDuro tipo, int capacidadEnGB) {
         this.marca = marca;
         this.tipo = tipo;
         this.capacidadEnGB = capacidadEnGB;
--- Objetos/Ejercicio2/src/main/java/Main.java
+++ Objetos/Ejercicio2/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
                     new PlacaBase("ASUS", "B650", "AM5"),
                     new Procesador("AMD", "Ryzen 7 7700X", 8, 4.5f, "AM5"),
                     new TarjetaGrafica("NVIDIA", "RTX 4070", 5888, 1.92f, 12),
-                    new DiscoDuro("Samsung", TIpoDiscoDuro.SSD, 1000)
+                    new DiscoDuro("Samsung", TipoDiscoDuro.SSD, 1000)
 
             );
 
@@ -20,7 +20,7 @@ public class Main {
                     new PlacaBase("MSI", "Z790", "LGB1700"),
                     new Procesador("Intel", "Core i7-14700K", 20, 3.4f, "LGA1700"),
                     new TarjetaGrafica("AMD", "RX 7800 XT", 3840, 2.12f, 16),
-                    new DiscoDuro("Seagate", TIpoDiscoDuro.HDD, 2000)
+                    new DiscoDuro("Seagate", TipoDiscoDuro.HDD, 2000)
             );
 
         } catch (OrdenadorException e) {
