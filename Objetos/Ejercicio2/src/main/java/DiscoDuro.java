1626aae0a197efcf67ba7dd5cdb612a181c02aff
Your Name <you@example.com>
2026-06-30 16:42:51 +0200

    Cambios

--- Objetos/Ejercicio2/src/main/java/DiscoDuro.java
+++ Objetos/Ejercicio2/src/main/java/DiscoDuro.java
@@ -8,4 +8,28 @@ private int capacidadEnGB;
         this.tipo = tipo;
         this.capacidadEnGB = capacidadEnGB;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public TipoDiscoDuro getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoDiscoDuro tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCapacidadEnGB() {
+        return capacidadEnGB;
+    }
+
+    public void setCapacidadEnGB(int capacidadEnGB) {
+        this.capacidadEnGB = capacidadEnGB;
+    }
 }
